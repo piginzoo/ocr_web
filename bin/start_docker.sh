6a612ee7c74a203e954e12785351e6605d8dc37a
piginzoo <piginzoo@gmail.com>
2019-09-11 16:09:07 +0800

    update

--- bin/start_docker.sh
+++ bin/start_docker.sh
@@ -3,3 +3,4 @@ docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=1 -t --rm -p 8501:8501 -p 
  --mount type=bind,source=/app/models/ctpn/pb,target=/models/ctpn \
  --mount type=bind,source=/app/models/config/models.cfg,target=/models/models.cfg \
  tensorflow/serving:1.14.0-gpu --model_config_file=/models/models.cfg
+
